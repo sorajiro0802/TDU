2ee1222dc4b15571d8e28cf1eff68a5673446b53
sorajiro0802 <sora_potato_0802@icloud.com>
2022-04-21 16:36:59 +0900

    add cs programs

---
+++ programEngineering/day2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace day2_1 {
+    class MainClass {
+        public static void Main(string[] args) {
+            Console.Write("文字列を入力してください:");
+            string s = Console.ReadLine();
+            Console.WriteLine("入力された文字列：{0}", s);
+        }
+    }
+}
---
+++ programEngineering/day2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace day2_2 {
+    class MainClass {
+        public static void Main(string[] args) {
+            var BackgroundColor = ConsoleColor.Blue;
+            Console.Write("ハロー・ワールド", BackgroundColor);
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.SetCursorPosition(0, 0);
+            //Console.Beep(3000, 1);
+            Console.ResetColor();
+        }
+    }
+}
