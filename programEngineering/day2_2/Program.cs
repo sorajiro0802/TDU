3009cb546376b5e3fb789d7318c254fef844f39d
sorajiro0802 <sora_potato_0802@icloud.com>
2022-04-22 15:20:47 +0900

    test

--- programEngineering/day2_2/Program.cs
+++ programEngineering/day2_2/Program.cs
@@ -3,13 +3,11 @@
 namespace day2_2 {
     class MainClass {
         public static void Main(string[] args) {
-            var BackgroundColor = ConsoleColor.Blue;
-            Console.Write("ハロー・ワールド", BackgroundColor);
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-            Console.SetCursorPosition(0, 0);
-            //Console.Beep(3000, 1);
-            Console.ResetColor();
-        }
+            for (int i = 0; i < 10; i++) {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Game Over");
+                Console.Beep();
+                }
+            }
     }
 }
