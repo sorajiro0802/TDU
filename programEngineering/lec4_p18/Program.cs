83106b9b7fa0af8ab15b29f1d255e19de54a375b
sorajiro0802 <sora_potato_0802@icloud.com>
2022-06-13 13:27:58 +0900

    add lec4

---
+++ programEngineering/lec4_p18/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lec4_p18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
