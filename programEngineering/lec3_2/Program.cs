f84ad3d78e6f9e579c4b03bc3d175ed78911eed2
sorajiro0802 <sora_potato_0802@icloud.com>
2022-06-13 12:33:21 +0900

    冗長なファイル

--- programEngineering/lec3_2/Program.cs
+++ programEngineering/lec3_2/Program.cs
@@ -4,6 +4,7 @@ namespace lec3_2 {
     class MainClass {
         public static void Main(string[] args) {
             // my ans
+            /*
             Random random = new Random();
             int i = random.Next(1, 10);
 
@@ -17,23 +18,21 @@ namespace lec3_2 {
                     Console.WriteLine("不正解");
                     i = random.Next(1, 10);
                 }
-            }
+            } */
             
             // teacher ans
-            /*
             Random random = new Random();
             int i = random.Next(1, 10);
-
             while (true) {
-                Console.WriteLine(i);
                 if (Console.ReadLine() != i.ToString()) {
                     Console.WriteLine("不正解");
                 }
                 else {
                     Console.WriteLine("正解");
+                    break;
                 }
             }
-            */
+            
         }
     }
 }
