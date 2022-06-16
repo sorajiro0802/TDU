82e76f81c4da1f77685754c5c132744d075a4261
sorajiro0802 <sora_potato_0802@icloud.com>
2022-06-16 19:51:58 +0900

    List test

---
+++ programEngineering/lec5_p9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lec5_p9
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            List<int> tList = new List<int>();            
+        }
+    }
+}
+
