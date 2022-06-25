3d56140fe6b0dcfdccf91b2fdd59f68c3ba3fc17
sorajiro0802 <sora_potato_0802@icloud.com>
2022-06-25 16:20:17 +0900

    update to comfirm List

--- programEngineering/lec5_p9/Program.cs
+++ programEngineering/lec5_p9/Program.cs
@@ -6,7 +6,42 @@ namespace lec5_p9
     class Program
     {
         static void Main(string[] args) {
-            List<int> tList = new List<int>();            
+            List<int> tList = new List<int>();
+            int addNum, insNum, delIndexNum, indexNum = 0;
+
+            while (true) {
+                Console.Write("select action[0~2]\n 0:add 1:insert 2:delete\n>> ");
+                string act = Console.ReadLine();
+
+                if (act == "0") {
+                    Console.Write("How?: ");
+                    addNum = int.Parse(Console.ReadLine());
+                    tList.Add(addNum);
+                }
+                else if (act == "1") {
+                    Console.Write("How?: ");
+                    insNum = int.Parse(Console.ReadLine());
+                    Console.Write("Where?: ");
+                    indexNum = int.Parse(Console.ReadLine());
+                    tList.Insert(indexNum, insNum);
+                }
+                else if (act == "2") {
+                    Console.Write("Where?: ");
+                    delIndexNum = int.Parse(Console.ReadLine());
+                    tList.RemoveAt(delIndexNum);
+                }
+                else {
+                    Console.WriteLine("\nOut of range!!!\nGoodBye :)");
+                    break;
+                }
+
+                Console.WriteLine("\nCurrent List is");
+                int n = tList.Count;
+                for (int i = 0; i < n; i++) {
+                    Console.WriteLine($"[{i}]:{tList[i]}\n");
+                }
+            }
+
         }
     }
 }
