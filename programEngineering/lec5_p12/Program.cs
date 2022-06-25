3bee516d3989c5baf03ec0c938d5e284b905f821
sorajiro0802 <sora_potato_0802@icloud.com>
2022-06-25 16:21:24 +0900

    予習とテスト

---
+++ programEngineering/lec5_p12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lec5_p12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            string name;
+
+            while (true) {
+                Console.Write("1:入力  2:終わり\n>> ");
+                string selectAct = Console.ReadLine();
+
+                switch (selectAct) {
+                    case "1":
+                        Console.Write("名前: ");
+                        name = Console.ReadLine();
+                        names.Add(name);
+                        break;
+
+                    default:
+                        break;
+                }
+                int n = names.Count;
+                for (int i = 0; i < n; i++) {
+                    Console.Write($"{names[i]} ");
+                }
+                Console.WriteLine("");
+                if(selectAct != "1") { break; }
+            }
+            
+        }
+    }
+}
+
---
+++ programEngineering/mini_test6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace mini_test6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 0;
+            int[] data1 = new int[] { 1, 3, 5, 7 };
+            int[] data2 = new int[] { 2, 4, 6, 8 };
+
+            for(int i = 0; i < data1.Length; i++) {
+                answer += Multiplication(data1[i], data2[i]);
+            }
+
+            Console.WriteLine($"Result is {answer}");
+        }
+
+        static int Multiplication(int num1, int num2) {
+            return num1 * num2;
+        }
+    }
+}
+
