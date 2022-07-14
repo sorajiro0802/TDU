6e74547428dc56655899cd4a84bac302be75960a
sorajiro0802 <sora_potato_0802@icloud.com>
2022-07-14 16:49:22 +0900

    complete code

--- programEngineering/lec6_p6/Program.cs
+++ programEngineering/lec6_p6/Program.cs
@@ -1,7 +1,23 @@
 ﻿namespace communication {
 class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World");
+            string printString = InputString();
+            int printTime = InputTime();
+            Print(printTime, printString);
+        }
+
+        static string InputString() {
+            Console.Write("文字表示列?: ");
+            return Console.ReadLine();
+        }
+        static int InputTime() {
+            Console.Write("表示回数?: ");
+            return int.Parse(Console.ReadLine());
+        }
+        static void Print(int printTime, string printString) {
+            for(int i = 0; i < printTime; i++) {
+                Console.WriteLine(printString);
+            }
         }
     }
 }
